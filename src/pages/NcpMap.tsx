69334889f02f26c4f0a04da1d80269aa236b6e21
Hwang Minji <hminji4565@gmail.com>
2025-03-06 09:35:43 +0900

    fix: build error 수정

--- src/pages/NcpMap.tsx
+++ src/pages/NcpMap.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { Pet } from "@/types/pet";
+import {useRadius} from "@/contexts/RadiusContext.tsx";
 
 interface NcpMapProps {
   currentLocation: {
